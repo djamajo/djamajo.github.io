dee061916291bd41aec8416a35b6ca4ffec3762c
Djama Mocke Joel <djama.joel@outlook.com>
2018-01-22 09:21:55 +0000

    files uploaded
    whole new design

--- assets/js/sidenav.js
+++ assets/js/sidenav.js
@@ -9,3 +9,12 @@ $(document).ready(function() {
               }
           });
       });
+
+    //   smooth scrolling
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
+    
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 500);
+    });
